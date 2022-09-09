c91f444c511d084bf748035c9acfe571c65e87d5
Josh Murray <joshmurray@Joshs-MacBook-Pro.local>
2022-09-09 14:36:36 -0600

    added search bar

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('#inpt');
-const button = document.querySelector('#searchBtn');
-const header = document.querySelector('.project-header');
+// const input = document.querySelector('#inpt');
+// const button = document.querySelector('#searchBtn');
+// const header = document.querySelector('.project-header');
 // var footer = document.querySelector('footer');
 
 
@@ -8,7 +8,7 @@ const loc = document.querySelector('#location');
 const tempF = document.querySelector('.f');
 const desc = document.querySelector('.desc');
 const humid = document.querySelector('.humidity');
-const wind = document.querySelector('.windspeed');
+const speed = document.querySelector('.windspeed');
 
 const api = 'eee36fb31545951ee6937b387f4442d1';
 
@@ -16,21 +16,22 @@ const api = 'eee36fb31545951ee6937b387f4442d1';
 
 
 
-// function searchBar(event){
-//     event.preventDefault();
-//     console.log(input.value);
-//     input.style.display= "none";
-//     button.style.display= "none";
-//     header.style.display= "none";
-//     footer.style.display= "none";
-//     var api = `https://api.openweathermap.org/data/2.5/weather?lat=${input.value}&api_key=${api}`;
+function searchBar(event){
+    event.preventDefault();
+    console.log(input.value);
+    input.style.display= "none";
+    button.style.display= "none";
+    header.style.display= "none";
+    footer.style.display= "none";
+    var api = `https://api.openweathermap.org/data/2.5/weather?lat=${input.value}&api_key=${api}`;
 
-// fetch(api)
-// .then(function(response){
-//     return response.json()
+fetch(api)
+.then(function(response){
+    return response.json()
 
     
-// })
+})
+}
 
 
 
@@ -56,10 +57,10 @@ window.addEventListener('load', () => {
             const { temp } = data.main;
             const { humidity } = data.main;
             const place = data.name;
-            const  wind  = data.wind;
-            const { description, icon } = data.weather[0];
+            const  { speed }  = data.wind;
+            const { description } = data.weather[0];
   
-            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            
             const fahrenheit = (temp * 9) / 5 + 32;
 
 
@@ -67,7 +68,8 @@ window.addEventListener('load', () => {
             desc.textContent = `${description}`;
             tempF.textContent = `${fahrenheit.toFixed(2)}°F `;
             humid.textContent = ` Humidity: ${humidity}`;
-            wind.textContent = `Wind Speed: ${wind.toFixed}`;
+            speed.textContent = ` Wind Speed: ${speed}`;
+
 
             
   
